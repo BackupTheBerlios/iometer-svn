63b15070e3b03e701e19b5852b1af2c30479b92e
xca1019 <xca1019@a98384dd-210c-0410-bdec-947c29cdc3f9>
2004-07-29 23:28:11 +0000

    Preprocessing for a new version of Iometer

--- branches/IOMETER1/iometer/src/IOVersion.h
+++ branches/IOMETER1/iometer/src/IOVersion.h
@@ -49,7 +49,9 @@
 /* ##                                                                     ## */
 /* ## ------------------------------------------------------------------- ## */
 /* ##                                                                     ## */
-/* ##  Changes ...: 2003-12-16 (daniel.scheibli@edelbyte.org)             ## */
+/* ##  Changes ...: 2004-07-30 (daniel.scheibli@edelbyte.org)             ## */
+/* ##               - Changed the version to current date.                ## */
+/* ##               2003-12-16 (daniel.scheibli@edelbyte.org)             ## */
 /* ##               - Changed the version to current date.                ## */
 /* ##               2003-10-15 (daniel.scheibli@edelbyte.org)             ## */
 /* ##               - Moved to the use of the IOMTR_[OSFAMILY|OS|CPU]_*   ## */
@@ -106,7 +108,8 @@ extern "C"
 //#define IOVER_FILEVERSION				"2003.05.10\0"
 //#define IOVER_FILEVERSION				"2003.05.10-post\0"
 //#define IOVER_FILEVERSION				"2003.12.16\0"
-#define IOVER_FILEVERSION				"2003.12.16-post\0"
+//#define IOVER_FILEVERSION				"2003.12.16-post\0"
+#define IOVER_FILEVERSION				"2004.07.30-RC1\0"
 #define IOVER_PRODUCTVERSION				IOVER_FILEVERSION
 
 
